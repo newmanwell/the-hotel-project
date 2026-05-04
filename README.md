6c336817e99e2ca5c57174c470a38fb1bad97395
Shaun Newman <shaun.newman.p@gmail.com>
2026-05-03 22:17:51 -0400

    chore: initial commit with base react with vite

---
+++ README.md
@@ -0,0 +1,2 @@
+# The Hotel Project
+* A new project to use TypeScript with React
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <section>
+       <h1>The Hotel Project</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
