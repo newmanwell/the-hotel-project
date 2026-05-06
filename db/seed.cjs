eb2cdf1c65359fc7c5c618b22f6be9e951d68719
Shaun Newman <shaun.newman.p@gmail.com>
2026-05-06 16:30:03 -0400

    chore: added colums to the hotels table and added mock data to the colums

--- db/seed.cjs
+++ db/seed.cjs
@@ -1,4 +1,15 @@
 const client = require('./client.cjs');
+const { createHotels } = require('./hotels.cjs');
+
+const deleteTables = async() => {
+  try {
+    await client.query(`
+        DROP TABLE IF EXISTS hotels;
+      `)
+  } catch(err) {
+    console.log(err);
+  }
+}
 
 const createTables = async() => {
   try {
@@ -18,10 +29,21 @@ const createTables = async() => {
 const initDb = async() => {
   await client.connect();
   console.log('Connected to hotelproject DB');
+
+  console.log('Deleting Tables');
+  await deleteTables();
+  console.log('Tables Deleted');
+  
   
   console.log('Adding Tables');
   await createTables();
   console.log('Tables Added');
+
+  console.log('Creating Hotels');
+  await createHotels('Hotel 1', 10000, 'Hotel 1 Description');
+  await createHotels('Hotel 2', 12500, 'Hotel 2 Description');
+  await createHotels('Hotel 3', 15000, 'Hotel 3 Description');
+  console.log('Hotels Created');
 
   await client.end();
   console.log('Disconnected to hotelproject DB')
