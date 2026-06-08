770913343cb08e5db172cf8b2353be45baac1ea1
Shaun Newman <shaun.newman.p@gmail.com>
2026-06-08 12:17:18 -0700

    chore: added nodemon and error handler

--- server.cjs
+++ server.cjs
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res, next) => {
-  res.send('Welcome to the jungle!')
+  res.send('Welcome to the jungle!');
 })
+
+
+app.use((req, res, next) => {
+  res.status(404).send('Page Not Found');
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err);
+});
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
